0b7b79ad57d7dc0e5cd44966a8930f96c327f61e
Adriel <addrriel@hotmail.com>
2025-11-03 04:58:33 -0500

    diseño de formulario completo

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  showPassword: boolean = false;
 
+  togglePasswordVisibility(event: Event) {
+    event.preventDefault();
+    this.showPassword = !this.showPassword;
+    const passwordInput = document.getElementById('password-input') as HTMLInputElement;
+    if (passwordInput) {
+      passwordInput.type = this.showPassword ? 'text' : 'password';
+    }
+  }
 }
