c8ee041707a49b85b0dda834d411d5d08bf799af
Adriel <addrriel@hotmail.com>
2025-11-03 12:20:02 -0500

    Solo navbar con login, mejorar navbar según el ejemplo

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,15 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  showPassword: boolean = false;
-
-  togglePasswordVisibility(event: Event) {
-    event.preventDefault();
-    this.showPassword = !this.showPassword;
-    const passwordInput = document.getElementById('password-input') as HTMLInputElement;
-    if (passwordInput) {
-      passwordInput.type = this.showPassword ? 'text' : 'password';
-    }
-  }
-}
+export class LoginComponent {}
